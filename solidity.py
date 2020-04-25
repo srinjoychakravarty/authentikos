8195e019161e409bf349504d3e20975e455874ec
Srinjoy Chakravarty <chakravarty.s@husky.neu.edu>
2020-04-24 20:44:33 -0400

    :construction: getter and setter functions enumerated and called or transacted as neccessary

--- solidity.py
+++ solidity.py
@@ -1,6 +1,13 @@
 from web3 import Web3, HTTPProvider
 import binascii, contract_abi, inquirer, json, pprint, subprocess, sys, time
 
+def get_chain_id(network):
+    with open('network_ids.json') as json_file: # Opening JSON file
+        chains = json.load(json_file)
+        chain_item = next((item for item in chains if item["name"] == network), None)
+        chain_id = int(chain_item.get('chainId'), 16)
+        return chain_id
+
 def setup():
     '''sets up connection to solidity smart contract'''
     network = input("Enter blockchain network: ") or "Rinkeby"
@@ -19,7 +26,7 @@ def setup():
     https_rinkeby = "https://rinkeby.infura.io/v3/" + infura_key
     w3 = Web3(HTTPProvider(https_rinkeby))
     contract = w3.eth.contract(address = contract_address, abi = contract_abi.abi)
-    return (wallet_address, w3, ws3, contract)
+    return (wallet_address, w3, ws3, contract, chain_id, wallet_private_key)
 
 def list_functions(contract):
     '''lists all smart contract functions '''
@@ -33,51 +40,42 @@ def enumerate_functions(all_functions):
     chosen_function = answer['function']
     return chosen_function
 
-# print(contract.functions.countAgencies().call())
-# print(contract.functions.contractOwner().call())
-# print(contract.functions.getAgencies().call())
-
-# agency3 = ("ndtv.com", "0x32804f2B543f4EbEce478D9847d8446650840128")
-# agency4 = ("cnbc.com", "0x6aED3Ca3C77a75Dc8d36ce9c306eA2A7aef576e0")
-# setAgency(string _address, address _ethIdentity)
-
-# txn_dict = contract.functions.setAgency(agency3[0], agency3[1]).buildTransaction({
-#     'chainId': 4,
-#     'gas': 140000,
-#     'gasPrice': w3.toWei('40', 'gwei'),
-#     'nonce': nonce,
-# })
-
-# signed_txn = w3.eth.account.signTransaction(txn_dict, private_key = wallet_private_key)
-# txn_hash_hex = (w3.eth.sendRawTransaction(signed_txn.rawTransaction)).hex()
-
-def get_chain_id(network):
-    with open('network_ids.json') as json_file: # Opening JSON file
-        chains = json.load(json_file)
-        chain_item = next((item for item in chains if item["name"] == network), None)
-        chain_id = int(chain_item.get('chainId'), 16)
-        return chain_id
-
-if __name__ == '__main__':
-    wallet_address, w3, ws3, contract = setup()
-    all_functions = list_functions(contract)
-    chosen_function = enumerate_functions(all_functions)
+def execute_function(chosen_function):
+    '''calls getter functions or executes setter functions via ether transactions'''
     if (chosen_function[0:3] == "get"):
         getter = \
         '''contract.functions.''' + chosen_function + '''.call()'''
-        print(eval(getter))
+        result_from_getter = eval(getter)
+        print(f"{chosen_function} called successfully!\nResult: {str(result_from_getter)}")
     else:
         nonce = w3.eth.getTransactionCount(wallet_address)
         params = chosen_function[chosen_function.find('(')+1 : chosen_function.find(')')]
         arguments = params.split(',')
-        
+        number_of_args = len(arguments)
         if 'address' in arguments:
             address_at_index = arguments.index("address")
+        user_inputs = []
+        for x in range(1, number_of_args + 1):
+            user_inputs.append(input(f"Please enter Parameter {x}: "))
+        txn_details = {'chainId': chain_id, 'gas': 140000, 'gasPrice': w3.toWei('40', 'gwei'), 'nonce': nonce}
+        chosen_method = chosen_function.split('(')[0]
+        setter = \
+        '''contract.functions.''' + chosen_method + '("' + user_inputs[0] + '", "' + user_inputs[1] + '")' + '''.buildTransaction(''' + str(txn_details) + ')'
+        txn_dict = eval(setter)
+        signed_setter = \
+        '''(w3.eth.account.signTransaction(''' + str(txn_dict) + ', private_key = "' + wallet_private_key + '")).rawTransaction'
+        signed_txn = eval(signed_setter)
+        sent_txn_cmd = \
+        '''w3.eth.sendRawTransaction(''' + str(signed_txn) + ')'
+        txn_hash = eval(sent_txn_cmd)
+        construct_hex = \
+        '''(''' + str(txn_hash) + ').hex()'
+        txn_hash_hex = "0x" + eval(construct_hex)
+        print(txn_hash_hex)
+        print(f"{chosen_method}({user_inputs[0]}, {user_inputs[1]}) executed successfully!\nTransaction hash: {txn_hash_hex}")
 
-        print(address_at_index)
-        # Web3.isAddress('0xd3CdA913deB6f67967B99D67aCDFa1712C293601')
-        #
-        # input_params = []
-        # for arg in arguments:
-        #     input_params.append(input("Enter parameter [" + arg + "]: "))
-        # print(input_params)
+if __name__ == '__main__':
+    wallet_address, w3, ws3, contract, chain_id, wallet_private_key = setup()
+    all_functions = list_functions(contract)
+    chosen_function = enumerate_functions(all_functions)
+    execute_function(chosen_function)
