31121456b07c3d144085c22e9d2e35d8354db8d6
Srinjoy Chakravarty <chakravarty.s@northeastern.edu>
2020-05-08 07:30:28 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,6 @@
 sudo chmod +x apt_bdb.sh
 sudo chmod +x venv_pip.sh
 echo "Installing all python3 prerequisites and bigchaindb for Authentikos..."
-x-terminal-emulator -e bash ./apt_bdb.sh
-return
+x-terminal-emulator -e bash ./apt_bdb.sh && exit
 echo "Installing required pip3 packages for Authentikos..."
-x-terminal-emulator -e bash ./venv_pip.sh 
+x-terminal-emulator -e bash ./venv_pip.sh && exit
